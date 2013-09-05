9a028bc8f3e0ef95b2e7efe982dcabccc939c982
Andrew Williams <andy@andywilliams.me>
2013-09-05 21:53:47 +0100

    Commiting rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shineget::Application.config.secret_key_base = 'd3832353143c0bc01b546b95ba0751a004e957f8266e65bfe4268186d6eb88b76b6f2ede6e141fdd57888f255c06cbe554ae30f08d7cadcc7503eeef3ec0a226'
