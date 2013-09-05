c45b08bd289a357386be346721f8d720df782655
Andrew Williams <andy@andywilliams.me>
2013-09-05 23:18:36 +0100

    Add a responsive bootstrap layout and style

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.0'
 gem 'will_paginate', '3.0.4'
 
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'bootstrap-will_paginate', '0.0.9'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Shineget::Application.routes.draw do
 
   resources :users
 
+  get "static_pages/home"
+
+  root 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
