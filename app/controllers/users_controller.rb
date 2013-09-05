5cc53cafb8ce6d638dbb66eb4f282f63683aba34
Andrew Williams <andy@andywilliams.me>
2013-09-05 22:29:21 +0100

    And add an items list to the user screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    @items = @user.items.paginate(page: params[:page])
   end
 
   # GET /users/new
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+  validates :user_id, presence: true
 end
