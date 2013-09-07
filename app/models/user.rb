ba23b83ecd1e9ab0db41513c93009ec9ec6c819d
Andrew Williams <andy@andywilliams.me>
2013-09-07 22:51:08 +0100

    Fixes post merge and more user validation. Create a default list for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ActiveRecord::Base
   has_many :lists
-
   before_save { self.email = email.downcase }
+
+  validates :username, presence: true
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
             format:     { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
-  has_secure_password
-  validates :password, length: { minimum: 6 }
+  after_create :add_default_list
+
+  protected
+    def add_default_list
+      lists.create(name:"Default Wishlist")
+      save
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without username" do
+    user = User.new
+    refute user.save, "Saved user without a username"
+  end
 end
