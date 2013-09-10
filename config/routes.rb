702d650ef21a08dc698bfa0e51cb79c996cefb12
Andrew Williams <andy@andywilliams.me>
2013-09-11 00:32:46 +0100

    Account menu

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Shineget::Application.routes.draw do
   match '/signup',  to: 'users#new',            via: 'get'
 
   match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
