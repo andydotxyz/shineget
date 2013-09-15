4a9e100dc2b2b104fc545840cf0a45baf2d4e469
Andrew Williams <andy@andywilliams.me>
2013-09-15 18:08:10 +0100

    Fix styling of lists for a user and some warnings in that code too

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.username, class: "gravatar")
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=120"
+    image_tag(gravatar_url, alt: user.username, class: 'gravatar')
   end
 end
