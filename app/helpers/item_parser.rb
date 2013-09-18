e0fa7b18f8a86251a0376946b7b5c033b3a68d54
Andrew Williams <andy@andywilliams.me>
2013-09-18 18:46:07 +0100

    parse firebox and tidy price detection too

--- app/helpers/item_parser.rb
+++ app/helpers/item_parser.rb
@@ -30,14 +30,39 @@ class ItemParser
     end
 
     def self.image_of_item(doc)
-      return doc.xpath('//img').select{|image| image.attribute("src").to_s.scan(/icon|CSS/).empty?}[0]['src']
+      return doc.xpath('//img').select{|image| image.attribute("src").to_s.scan(/icon|CSS|currency/).empty? &&
+          image.attribute("id").to_s.scan(/logo/).empty?
+      }[0]['src']
     end
 
     def self.price_of_item(doc)
-      price_text = doc.at('p:contains("£")')
+      # look for prices in things marked as class="text"
+      doc.css('.price').each do |node|
+        price = price_in_text node.text
+        if price > 0.0
+          return price
+        end
+      end
 
-      if price_text
-        return price_text.text.match(/(\d+[,.]\d+)/)
+      # search for prices in each paragraph
+      price = price_in_type('p', doc)
+      if price > 0
+        return price
+      end
+
+      # search for prices in each span
+      return price_in_type('span', doc)
+    end
+
+    def self.price_in_type(type, doc)
+      price_text = doc.xpath('//' + type + '[contains(text(), \'£\') or contains(text(), \'&pound;\') or contains(text(), \'&#163;\')]')
+
+      return price_in_text price_text.text
+    end
+
+    def self.price_in_text(text)
+      if text
+        return text.match(/(\d+[,.]\d+)/).to_s.to_f
       end
       return 0.0
     end
--- test/helpers/item_parser_test.rb
+++ test/helpers/item_parser_test.rb
@@ -17,11 +17,19 @@ class ItemParserTest < ActionView::TestCase
     assert_equal 24.99, item.price
   end
 
-  test 'parsing real life html gets correct data' do
+  test 'parsing real tshirt html gets correct data' do
     item = ItemParser.parse('file://' + File.dirname(__FILE__) + '/tshirt.html')
 
     assert_equal 'Men\'s Zelda Nintendo With Link T-Shirt (Black)', item.title
     assert_equal 'http://p.playserver1.com/ProductImages/0/3/5/9/5/2/9/3/39259530_300x300_1.jpg', item.imgurl
     assert_equal 16.99, item.price
+  end
+
+  test 'parsing real screwdriver html gets correct data' do
+    item = ItemParser.parse('file://' + File.dirname(__FILE__) + '/screwdriver.html')
+
+    assert_equal 'Tenth Doctor\'s Sonic Screwdriver Universal Remote Control', item.title
+    assert_equal 'http://media.firebox.com/pic/p6153_column_grid_6.jpg', item.imgurl
+    assert_equal 69.95, item.price
   end
 end
