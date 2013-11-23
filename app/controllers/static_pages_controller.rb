12b5e83b06baf05048dcf707f17a9784f415d5f6
Andrew Williams <andy@andywilliams.me>
2013-11-23 21:39:15 +0000

    Add support for custom domains

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
 
   def home
+    # deal with custom domains
+    if is_user_request?
+      redirect_to 'http://shineget.me/users/' + username_from_host
+      return
+    end
+
     if signed_in?
       redirect_to current_user
     end
@@ -11,6 +17,19 @@ class StaticPagesController < ApplicationController
 
   def admin
     redirect_to root_url, notice: "Permission denied" unless self.admin?self.current_user
+  end
+
+  #custom domain stuff
+  def is_user_request?
+    if request.host.length < 13
+      false
+    end
+
+    !User.find_by_username(username_from_host).nil?
+  end
+
+  def username_from_host
+    request.host.sub('.shineget.me', '')
   end
 end
 
