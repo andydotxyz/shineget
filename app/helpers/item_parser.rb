b0bd7c76dd293af30da89cd3cb10d0ce63f6597f
Andy Williams <andy@andywilliams.me>
2015-09-11 19:22:08 +0100

    Actually add the magic parameter that fixes redirections in lookup

--- app/helpers/item_parser.rb
+++ app/helpers/item_parser.rb
@@ -8,7 +8,7 @@ class ItemParser
     @item = Item.new
     @item.url = url_or_file
 
-    doc = Nokogiri::HTML(open(url_or_file))
+    doc = Nokogiri::HTML(open(url_or_file, :allow_redirections => :safe))
 
     @item.title = title_of_item doc
     @item.imgurl = image_of_item doc
